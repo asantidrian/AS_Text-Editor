ff3326d729af5497ef10f09d0d3a4f3f4f7707ba
asanti <asantidrian83@gmail.com>
2022-07-13 12:18:46 +1000

    datebase js file code added and plugins added to the webpack config file

---
+++ client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+//Method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
+
+//Method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+initdb();
